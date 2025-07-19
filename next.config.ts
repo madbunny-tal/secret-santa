b0e0afc77382e2c70b903e6fb0255e8f746510a6
madbunny-tal <chsntori86@gmail.com>
2025-07-19 14:16:04 +0700

    Config fix (from gh-deploy)

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,26 @@
 import type { NextConfig } from "next";
 import { hostname } from "os";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
+  assetPrefix: isProd ? '/secret-santa/' : '',
+  basePath: isProd ? '/secret-santa' : '',
   images: {
+    unoptimized: true,    
     remotePatterns: [{
-      protocol: 'https',
-      hostname:'mqichvjbjuhwmbtpnklj.storage.supabase.co',
-      pathname: '**'
-    },
-    {
-      protocol: 'https',
-      hostname:'mqichvjbjuhwmbtpnklj.supabase.co',
-      pathname: '**'
-    }]
+        protocol: 'https',
+        hostname:'mqichvjbjuhwmbtpnklj.storage.supabase.co',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname:'mqichvjbjuhwmbtpnklj.supabase.co',
+        pathname: '**'
+      }]
   }
 };
 
